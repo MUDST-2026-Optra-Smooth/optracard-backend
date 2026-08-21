82f37e2c410846715e61c2b03839d2bd96137eec
oKENDOo <kendokendo14271@gmail.com>
2026-08-21 11:19:44 +0700

    Initial backend setup with Spring Boot

---
+++ src/test/java/com/optracard/backend/OptracardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optracard.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptracardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
