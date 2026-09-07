599a2298abf89af134c4a707dcb944fe554ff6e7
Saksit Jittasopee <saksitjittasopee@gmail.com>
2026-09-07 18:04:49 +0700

    move user from entity to model

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package org.example.repository;
 
-import org.example.entity.User;
+import org.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
